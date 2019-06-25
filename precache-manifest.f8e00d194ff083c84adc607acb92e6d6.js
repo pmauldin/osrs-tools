dc9fe38074f47a24a13008c3af6480d6063639a6
Peter Mauldin <petermauldin1@gmail.com>
2019-06-24 20:26:49 -0500

    Updates

---
+++ precache-manifest.f8e00d194ff083c84adc607acb92e6d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3312fe11323f8d36020e76a499408a1e",
+    "url": "/osrs-tools/index.html"
+  },
+  {
+    "revision": "be418bcab8e2f3b6614c",
+    "url": "/osrs-tools/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/osrs-tools/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "be418bcab8e2f3b6614c",
+    "url": "/osrs-tools/static/js/main.e45eccdd.chunk.js"
+  },
+  {
+    "revision": "46993cbae8e382f15ab3",
+    "url": "/osrs-tools/static/js/runtime~main.ce79e23e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/osrs-tools/static/media/logo.5d5d9eef.svg"
+  }
+]);
